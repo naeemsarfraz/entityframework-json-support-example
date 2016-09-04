e320b8d557a2165f7ea2d9dbc516e0f2504003d9
Naeem Sarfraz <naeem.sarfraz@gmail.com>
2016-09-04 06:37:59 +0100

    and stubbed methods for the task at hand

--- EntityFramework.Json.Example/Program.cs
+++ EntityFramework.Json.Example/Program.cs
@@ -9,7 +9,24 @@ namespace EntityFramework.Json.Example
             MainAsync(args).Wait();
         }
 
-        static Task MainAsync(string[] args)
+        static async Task<int> MainAsync(string[] args)
+        {
+            using (var db = new TranslationContext())
+            {
+                await Seed(db);
+
+                await QueryExamples(db);
+            }
+
+            return 0;
+        }
+
+        private static Task QueryExamples(TranslationContext db)
+        {
+            return Task.FromResult(0);
+        }
+
+        private static Task Seed(TranslationContext db)
         {
             return Task.FromResult(0);
         }
